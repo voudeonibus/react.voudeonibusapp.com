e950a345f9121c315fcc5439b3d87001be2c429e
Lucas <lucas@agtlucas.com>
2015-10-07 12:25:38 -0300

    Update webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var ReactToHtmlPlugin = require('react-to-html-webpack-plugin');
+
+var path = require('path');
+var ejs = require('ejs');
+var fs = require('fs');
